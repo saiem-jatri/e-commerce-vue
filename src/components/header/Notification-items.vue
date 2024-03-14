e279529b0c829cd3fc26d255c9f1b35edc2eb5c9
saiem-jatri <93651392+saiem-jatri@users.noreply.github.com>
2024-03-14 15:40:14 +0600

    new arrival modal design updated

--- src/components/header/Notification-items.vue
+++ src/components/header/Notification-items.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-  <div class="w-full h-full">
+  <div class="w-full h-full z-50">
     <div @mouseover="cartOpen = true"  @mouseleave="cartOpen=false" class="relative w-14 h-14 hover:bg-[#8DB2EE] rounded-full flex justify-center items-center cursor-pointer transition-all ease-in-out">
       <slot ></slot>
       <div class="h-4 w-4 flex justify-center items-center bg-black text-white font-bold rounded-full absolute top-2 right-2">
--- src/components/homepageComponents/CategoriesComponent/NewArrival.vue
+++ src/components/homepageComponents/CategoriesComponent/NewArrival.vue
@@ -7,12 +7,11 @@ const emits = defineEmits(["closeModal"]);
 </script>
 
 <template>
-  <div class="w-[400px] bg-white absolute z-40 shadow-2xl top-10">
+  <div class="w-full bg-white absolute z-40 shadow-2xl top-10 left-0 right-0">
         <div class="grid grid-cols-2 md:grid-cols-5">
             <div class="flex justify-center items-center">
               <img src="../../../assets/images/newArrible.jpg" alt="">
             </div>
-          <button @mouseleave="emits('closeModal')" class="w-6 bg-blue-300">close</button>
         </div>
   </div>
 </template>
