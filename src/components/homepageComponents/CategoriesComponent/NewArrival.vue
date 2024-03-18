537e880e1d659ed1c036fd519ec8e5f2e812e8a3
saiem-jatri <93651392+saiem-jatri@users.noreply.github.com>
2024-03-18 14:55:37 +0600

    new arrival modal addded

--- src/components/homepageComponents/CategoriesComponent/NewArrival.vue
+++ src/components/homepageComponents/CategoriesComponent/NewArrival.vue
@@ -1,17 +1,50 @@
 <script setup>
 const props = defineProps([
   "modelValue",
-    "index"
+    "index",
+    'showingName'
 ]);
 const emits = defineEmits(["closeModal"]);
+
+const details = {
+  'men' : ['Formal-Shirt','Casual-shirt','SweatShirt','pant', 'panjabi'],
+  'women' : ['shari','three-pices','SweatShirt'],
+  'kids' : ['t-shirts','pant','panjabi'],
+  'newborn' : ['t-shirts','dyper','pants'],
+  'Accessories' : ['watch','sunglass','belt'],
+  'Footwear' : ['snikkers','shoe','casual shoe'],
+}
+
+
 </script>
 
 <template>
-  <div class="w-full bg-white absolute z-40 shadow-2xl top-10 left-0 right-0">
-        <div class="grid grid-cols-2 md:grid-cols-5">
-            <div class="flex justify-center items-center">
-              <img src="../../../assets/images/newArrible.jpg" alt="">
+  <div class="w-1/2 bg-white absolute z-40 shadow-black shadow-2xl top-10 left-1/2 transform -translate-x-1/2">
+        <div class="grid grid-cols-2">
+            <div class="flex justify-center items-center p-4">
+              <img class="relative" src="../../../assets/images/newArrible.jpg" alt="">
+              <div class="absolute top-4 right-[51.8%] w-32 h-32 bg-red-600 text-white italic text-sm z-20 flex flex-col justify-center items-center">
+                <div class="text-xl md:text-4xl font-extrabold italic shadow-2xl flex justify-center">
+                  <p class="text-white text-4xl flex">O <span class="text-black">llE</span></p>
+                </div>
+                <p class="text-xl md:text-2xl font-extrabold">
+                  New
+                </p>
+                <p class="text-xl md:text-3xl font-extrabold">Arrial</p>
+              </div>
             </div>
+          <div class="p-4">
+            <h1 class="text-xl font-bold col-span-3">Showing Items For {{ props.showingName }}</h1>
+
+              <ul v-for="value in details[props.showingName]" class="flex flex-col flex-wrap  gap-x-2 list-disc pl-4 mt-4">
+                <li class=""><router-link to="">
+                  {{value}}
+                </router-link>
+                </li>
+              </ul>
+
+
+          </div>
         </div>
   </div>
 </template>
--- src/components/homepageComponents/CategoriesComponent/categories.vue
+++ src/components/homepageComponents/CategoriesComponent/categories.vue
@@ -1,31 +1,36 @@
 <script setup>
-import {reactive, ref} from "vue";
+import {computed, reactive, ref} from "vue";
 import NewArrival from "./NewArrival.vue";
 const openModal = ref(false)
 
-const allCategories = reactive(['men','WOMEN','kids','newborn','Accessories','Footwear'])
+const allCategories = reactive(['men','women','kids','newborn','Accessories','Footwear'])
 const closeModal = () => {
   openModal.value = false;
 };
-const showModalCabin = () => {
+let showingName = ref([])
+
+const showModal = (i) => {
+  showingName.value = i
   openModal.value = true;
 };
+
 </script>
 
 <template>
   <div class="px-4 md:px-52 flex justify-center items-center mt-5 relative">
-      <div class="flex gap-x-4 flex-wrap justify-center items-center text-[#4d4d4d] text-xs md:text-sm font-semibold uppercase pb-2 border-b border-gray-200 w-full">
-          <div @mouseover="showModalCabin" v-for="(category) in allCategories" class="cursor-pointer z-50 hover:-translate-y-1 transition-all ease-in">{{ category }}</div>
+      <div class="flex gap-x-4 flex-wrap justify-center items-center text-[#4d4d4d] text-xs md:text-sm font-semibold uppercase pb-2 border-b border-gray-200 w-full z-50">
+          <div @mouseover="showModal(category)" v-for="(category) in allCategories" class="cursor-pointer z-50 hover:-translate-y-1 transition-all ease-in">{{ category }}</div>
       </div>
     <div>
       <transition name="slide-fade">
         <NewArrival
             v-if="openModal"
+            :showingName="showingName"
             @close-modal = closeModal
         />
       </transition>
       <div v-if="openModal" @click="openModal = false"
-           class="fixed bg-black bg-opacity-20 inset-0 z-20 h-full w-full cursor-default transition duration-300 ease-in-out">
+           class="fixed bg-black opacity-5 inset-0 z-20 h-full w-full cursor-default transition duration-300 ease-in-out">
       </div>
     </div>
   </div>
